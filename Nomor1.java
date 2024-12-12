ef80d012eebd2321de72660b318d8c0f38fa0295
Sharaanjelia <sharaanjelia236@gmail.com>
2024-12-12 09:16:24 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,9 +6,6 @@ public class Nomor1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        tampilkanAngkaGanjil(N);
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
-    }
         /* Pekerjaan anda mulai dari sini */
     public static void tampilkanAngkaGanjil(int n) {
         System.out.println("Angka ganjil dari 0 hingga " + n + ":");
@@ -17,5 +14,7 @@ public class Nomor1 {
                 System.out.println(i + " ");
             }
         }
+    System.out.println(Nomor1.class.getDeclaredMethods().length); 
+
     }
 }
